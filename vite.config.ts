12e9944f7bffb37185d0bdedaaa90e25643c277c
MontoyaCl3 <cmontoya2739@gmail.com>
2025-06-09 12:10:01 -0500

    run build 2

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
+ base: "/vibra-vista-dashboard",
   plugins: [
     react(),
     mode === 'development' &&
